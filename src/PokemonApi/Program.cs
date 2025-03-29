ea87ebd5a2a0f67b219363d6fc135a9791f9a11c
Samuel Coelho <samuel.scoelho@gmail.com>
2025-03-29 20:56:28 -0300

    Remove WeatherForecast reference

--- src/PokemonApi/Program.cs
+++ src/PokemonApi/Program.cs
@@ -20,26 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapGet("GetPokemon/{nameOrId}", async (string nameOrId) =>
 {
     var pokemon = await PokiApiClient.GetPokemon(nameOrId);
@@ -61,8 +41,3 @@ app.MapGet("GetPokemonPaginated", async (int? limit, int? offset) =>
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
