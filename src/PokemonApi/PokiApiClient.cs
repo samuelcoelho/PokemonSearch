3f93b1c481a104b8272b5b3f93a8ab3bf0b902c7
Samuel Coelho <samuel.scoelho@gmail.com>
2025-03-29 11:19:51 -0300

    Solution and ApiClient

---
+++ src/PokemonApi/PokiApiClient.cs
@@ -0,0 +1,23 @@
+﻿namespace PokemonApi;
+
+using System.Net.Http;
+
+public class PokiApiClient
+{
+    private static readonly string uriBasePokiApi = "https://pokeapi.co/api/v2/pokemon";
+    private static HttpClient httpClient = new HttpClient();
+
+    public static async Task GetPokemon(string searchParam)
+    {
+        httpClient.BaseAddress = new Uri(uriBasePokiApi);
+        string requestUri = uriBasePokiApi + "/" + searchParam;
+        using HttpResponseMessage response = await httpClient.GetAsync(requestUri);
+
+        var teste = response.StatusCode == System.Net.HttpStatusCode.OK;
+        response.EnsureSuccessStatusCode();
+        Console.WriteLine(response.Content);
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(jsonResponse);
+    }
+}
---
+++ src/PokemonApi/Program.cs
@@ -0,0 +1,62 @@
+using System.Reflection.Metadata.Ecma335;
+using PokemonApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/hello", () =>
+{
+    return "Hello";
+})
+.WithName("GetHello")
+.WithOpenApi();
+;
+
+app.MapGet("GetPokemon", async () =>
+{
+    await PokiApiClient.GetPokemon("pikachu");
+})
+.WithName("GetPokemon")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
